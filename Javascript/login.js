8c2feed11c2809cac861b8f011a6e5a3ad64a466
ISMAEL KAGOU <ismaelkagou@gmail.com>
2024-11-20 04:55:06 +0100

    third

---
+++ Javascript/login.js
@@ -0,0 +1,4 @@
+document.querySelector('.login form').addEventListener('submit', function(event) {
+    // Show a simple alert (you can replace this with more complex logic)
+    alert('Logging in...');
+});
