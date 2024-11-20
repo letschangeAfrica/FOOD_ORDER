21285654e488d8b9e70eccf976a48729dec2a19a
ISMAEL KAGOU <ismaelkagou@gmail.com>
2024-11-20 04:45:30 +0100

    first

---
+++ contact.php
@@ -0,0 +1,130 @@
+<?php include('partial-front/menu.php'); ?>
+<link rel="stylesheet" href="css/swiper-bundle.min.css">
+<link rel="stylesheet" href="css/contact.css">
+<!-- fOOD sEARCH Section Starts Here -->
+<section class="food-search">
+    <video autoplay loop muted>
+        <source src="images/79637-570129423_tiny.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</section>
+<section class="about-us-section">
+    <div class="container">
+        <h2 class="text-center">About Us</h2>
+
+        <div class="about-content">
+            <p>Welcome to our campus restaurant website! We are a dedicated team of Sophomore and Junior students from the Computing and Software Engineering department, and We are thrilled to share our journey with you. Over the course of just one month during the Fall 2024 semester, we poured our hearts and souls into this project as part of our Web Development class.</p>
+
+            <p>Our team utilized a combination of technologies, including HTML, CSS, JavaScript, PHP, and MySQL, to bring our vision to life. This project is not just a restaurant website; it is a testament to our hard work, creativity, and collaboration. We are immensely grateful to our school for providing us with the opportunity to build this project, and we would like to extend a special thank you to Ms. Reine and her team for their unwavering dedication in making the construction of our dream a reality.</p>
+
+            <p>This endeavor serves as proof that within the Computing and Software Engineering department, we have the potential to create remarkable things. All we need are opportunities to express ourselves and showcase our skills. We would also like to express our gratitude to Mr. Kakeu, our lecturer, and the administration for their support throughout this journey.</p>
+
+            <p>We invite you to visit us and experience the fruits of our labor. If you have any questions or would like to reach out, please do not hesitate to contact us at any time. Thank you for being a part of our story!</p>
+        </div>
+    </div>
+</section>
+<section class="slider-section">
+    <h2 class="text-center">Team</h2>
+    <div class="slide-container swiper">
+        <div class="slide-content">
+            <div class="card-wrapper swiper-wrapper">
+                <!-- Card 1 -->
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+
+                        <div class="card-image">
+                            <img src="images/ismael.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ismaël KAGOU</h2>
+                        <p class="description"> Sophomore II, Future Data Professional Leader</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="images/ilona.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ilona KEMGO</h2>
+                        <p class="description"> Sophomore II, Aspiring Data Analyst</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="images/ivan.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ivan NGAKU</h2>
+                        <p class="description"> Junior II, IT Student</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="images/cookies.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ismaël KAGOU</h2>
+                        <p class="description"> Sophomore II, Future Data Professional Leader</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="images/chinchin.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ismaël KAGOU</h2>
+                        <p class="description"> Sophomore II, Future Data Professional Leader</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="images/momo.jpg" alt="" class="card-img">
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <h2 class="name">Ismaël KAGOU</h2>
+                        <p class="description"> Sophomore II, Future Data Professional Leader</p>
+
+                        <button class="button"> View More</button>
+                    </div>
+                </div>
+
+                <!-- Add as many cards as needed -->
+            </div>
+        </div>
+        <div class="swiper-button-next swiper-navBtn"></div>
+        <div class="swiper-button-prev swiper-navBtn"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+</section>
+<!-- fOOD sEARCH Section Ends Here -->
+<script src="Javascript/swiper-bundle.min.js"></script>
+<script src="Javascript/contact.js"></script>
+
+<?php include('partial-front/footer.php'); ?>
