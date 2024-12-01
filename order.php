c3595d562e50e9e759e04c1ed719d9ffec78a1b4
ISMAEL KAGOU <ismaelkagou@gmail.com>
2024-12-01 01:12:12 +0100

    new design

--- order.php
+++ order.php
@@ -205,7 +205,6 @@
     }
 
 </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 <script>
   window.onload = function () {
     // Check if the order was successfully placed
--- partial-front/menu.php
+++ partial-front/menu.php
@@ -9,6 +9,7 @@
 
     <!-- Link our CSS file -->
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 </head>
 
 <body>
