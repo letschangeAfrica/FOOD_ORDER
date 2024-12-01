03f5534974cdac307c454cac549f59c77ef5ff13
ISMAEL KAGOU <ismaelkagou@gmail.com>
2024-12-01 01:41:17 +0100

    new design

--- partial-front/menu.php
+++ partial-front/menu.php
@@ -9,7 +9,7 @@
 
     <!-- Link our CSS file -->
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js"></script>
+    <script src="jsPDF-master/dist/jspdf.umd.min.js"></script>
 </head>
 
 <body>
